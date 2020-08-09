23b138f097524f5eaf36b5ca29c126173501afd9
wanglinke <2509450456@qq.com>
2020-08-09 20:17:34 +0800

    wlkwlkwlk

--- src/main/java/com/fh/Student/Student.java
+++ src/main/java/com/fh/Student/Student.java
@@ -3,5 +3,6 @@ package com.fh.Student;
 public class Student {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("**********");
     }
 }
---
+++ src/main/java/com/fh/Student/Teacher.java
@@ -0,0 +1,4 @@
+package com.fh.Student;
+
+public class Teacher {
+}
